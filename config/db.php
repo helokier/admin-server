17e0c0edfcc5417a0580356bb21a1fc5841cb65b
helokier <55515510+helokier@users.noreply.github.com>
2021-10-01 21:18:53 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 $host  = "localhost";
-$username = "root";
-$password = "";
-$database = "admin";
+$username = "a7a857_helokit";
+$password = "Truongtai1";
+$database = "db_a7a857_helokit";
 
 //Connecttion
 $con = mysqli_connect($host, $username, $password, $database);
