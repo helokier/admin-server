d5dc1818a4e0d1ee6c82f69ac99ae283785c411b
helokier <55515510+helokier@users.noreply.github.com>
2021-10-08 13:57:21 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host  = "mysql5031.site4now.net";
-$username = "a7a857_helokit";
-$password = "Truongtai_12";
-$database = "db_a7a857_helokit";
+$host  = "localhost";
+$username = "root";
+$password = "";
+$database = "admin";
 
 //Connecttion
 $con = mysqli_connect($host, $username, $password, $database);
