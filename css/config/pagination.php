26719e776b4addebdba95453c3c92259bb5aaf40
helokier <55515510+helokier@users.noreply.github.com>
2021-10-08 13:54:54 +0700

    update
    updateting

---
+++ css/config/pagination.php
@@ -0,0 +1,35 @@
+
+<?php
+include('config/db.php');
+  
+
+
+
+
+
+
+
+// // //paginations 
+// // $recornd_per_page = 5;
+// // $page = '';
+// // $output = '';
+// // if(isset($_POST['page'])){
+// //     $page = $_POST['page'];
+// // }else{
+// //     $page =1;
+// // }
+// // $start_from = ($page-1)*$recornd_per_page;
+// $limit = 3;
+// $page = isset($_POST['page']) ? $_POST['page'] : 1;
+// $start = ($page-1)*$limit;
+// $result = $con -> query("SELECT * FROM product LIMIT $start,$limit");
+// $customer = $result -> fetch_all(MYSQLI_ASSOC);
+
+
+// // $result1 = $con->query("SELECT count(id) AS id FROM product ");
+// // $custCount = $result->fetch_all(MYSQLI_ASSOC);
+// // $total = $custCount[0]['id'];
+// // $page = ceil($total / $limit);
+
+
+?>
